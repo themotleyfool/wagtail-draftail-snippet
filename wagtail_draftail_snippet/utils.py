387249204068358a1adfaa24e2bd50b2b693dbf2
Nick Travis <ntravis@fool.com>
2022-11-07 12:16:01 -0500

    converting from legacy % to f-strings

--- wagtail_draftail_snippet/utils.py
+++ wagtail_draftail_snippet/utils.py
@@ -1,8 +1,8 @@
 
 
 def get_snippet_link_frontend_template(app_name, model_name):
-    return "%s/%s_snippet_link.html" % (app_name, model_name)
+    return f"{app_name}/{model_name}_snippet_link.html"
 
 
 def get_snippet_embed_frontend_template(app_name, model_name):
-    return "%s/%s_snippet_embed.html" % (app_name, model_name)
+    return f"{app_name}/{model_name}_snippet_embed.html"
